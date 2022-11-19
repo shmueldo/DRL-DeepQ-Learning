029752f1e95473c27f96649cbcbb68fd3de0566a
Dor Haim Shmuel <shmueldo@post.bgu.ac.il>
2022-11-19 20:50:12 +0200

    in construction of DQN class

--- QAlgorithms.py
+++ QAlgorithms.py
@@ -10,12 +10,12 @@ SEED = 42
 np.random.seed(SEED)
 
 class QLearning(object):
-    def __init__(self, environment, learning_rate:float,
+    def __init__(self, environment, q_learning_rate:float,
                  discount_factor:float, decaying_rate:float,
                  epsilon:float):
         self.environment = environment
         self.q_values = self.initialize_q()
-        self.learning_rate = learning_rate
+        self.q_learning_rate = q_learning_rate
         self.discount_factor = discount_factor
         self.epsilon = epsilon
         self.decaying_rate = decaying_rate
@@ -36,7 +36,7 @@ class QLearning(object):
                                     loc ='upper left')
         ax.set_title('Q-value lookup table after {} episodes'.format(episode), fontweight ="bold")
     
-    def epsilon_greedy_action(self, current_state):
+    def sample_action(self, current_state):
             odd = np.random.choice(2, 1, p=[self.epsilon, 1 - self.epsilon])
             if odd == 1: return np.argmax(self.q_values[current_state, :])
             else: return self.environment.action_space.sample()
@@ -60,7 +60,7 @@ class QLearning(object):
         return target
             
     def q_update(self, current_state, current_action, target):
-        q = (1 - self.learning_rate) * self.q_values[current_state, current_action] + self.learning_rate * target
+        q = (1 - self.q_learning_rate) * self.q_values[current_state, current_action] + self.q_learning_rate * target
         err = self.q_values[current_state, current_action] - q
         self.q_values[current_state, current_action] = q
         return err
@@ -75,28 +75,54 @@ class ThreeLayersModel(tf.keras.Model):
     self.output_layer   = Dense(output_dim, activation='linear')
     self.dropout        = tf.keras.layers.Dropout(0.5)
 
-  def call(self, inputs, training=False):
-    x = self.dense1(inputs)
-    if training:
-      x = self.dropout(x, training=training)
-    return self.dense2(x)
+  def call(self, inputs):
+    x = self.input_layer(inputs)
+    x = self.hidden(x)
+    return self.output_layer(x)
 
 class DeepQLearning(QLearning):
-    def __init__(self, environment, learning_rate:float, discount_factor:float,
-                 decaying_rate:float, epsilon:float, model_type:tf.keras.Model):
-        super().__init__(environment, learning_rate, discount_factor,
+    def __init__(self, environment, q_learning_rate:float, discount_factor:float,
+                 decaying_rate:float, epsilon:float, model_type:tf.keras.Model,
+                 criterion, optimizer, net_learning_rate):
+        super().__init__(environment, q_learning_rate, discount_factor,
                          decaying_rate, epsilon)
+        self.exp_reply_deque    = deque(maxlen=50000)
+        self.main_model         = self.model_init(model_type, criterion, optimizer, net_learning_rate)
+        self.target_model       = self.model_init(model_type, criterion, optimizer, net_learning_rate)
         
-    def model_init(self, model_type, criterion, optimizer, learning_rate):
+    def input_reshape(self, input_state):
+        """ reshape single state input as a model input
+
+        Args:
+            input_state (state): the input state
+        """        
+        return input_state.reshape([1, input_state.shape[0]])
+    
+    def model_init(self, model_type, criterion, optimizer, net_learning_rate):
         model = model_type(self.dim[0], self.dim[1])
-        model.compile(loss= criterion, optimizer=optimizer(learning_rate), metrics=['mse'])
+        model.compile(loss= criterion, optimizer=optimizer(net_learning_rate), metrics=['mse'])
         return model
     
-    def epsilon_greedy_action(self, current_state):
+    def sample_action(self, current_state):
         odd = np.random.choice(2, 1, p=[self.epsilon, 1 - self.epsilon])
         if odd == 1:
-            return np.argmax(self.q_values[current_state, :])
+            return np.argmax(self.main_model.predict(self.input_reshape(current_state)).flatten()) 
         else:
             return self.environment.action_space.sample()
+
+    def sample_batch(self, batch_size):
+        return np.random.choice(self.exp_reply_deque, batch_size)
     
+    def store_past_exp(self, current_state, current_action, reward, done, next_state):
+        self.exp_reply_deque.append([current_state, current_action, reward, done, next_state])
     
+    def get_target(self, next_state, reward, done):
+        if not(done):
+            target = reward + self.discount_factor * np.max(self.target_model.predict(next_state))
+        else:
+            target = reward
+        return target
+    
+    def train_agent(self, batch_size):
+        # consider to apply condition on the size of the experience reply deque
+      
