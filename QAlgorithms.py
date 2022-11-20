497059a471105c70380d05012fecf3e9d4f128a9
Dor Haim Shmuel <shmueldo@post.bgu.ac.il>
2022-11-20 21:51:58 +0200

    DQN class + Models

--- QAlgorithms.py
+++ QAlgorithms.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Dense
 from collections import deque
 import numpy as np
 import gym
+from Models import *
 
 SEED = 42
 np.random.seed(SEED)
@@ -64,33 +65,28 @@ class QLearning(object):
         err = self.q_values[current_state, current_action] - q
         self.q_values[current_state, current_action] = q
         return err
-
-
-class ThreeLayersModel(tf.keras.Model):
-    
-  def __init__(self, input_dim, output_dim):
-    super().__init__()
-    self.input_layer    = Dense(32, input_shape=input_dim, activation='relu')
-    self.hidden         = Dense(16, activation='relu')
-    self.output_layer   = Dense(output_dim, activation='linear')
-    self.dropout        = tf.keras.layers.Dropout(0.5)
-
-  def call(self, inputs):
-    x = self.input_layer(inputs)
-    x = self.hidden(x)
-    return self.output_layer(x)
-
 class DeepQLearning(QLearning):
     def __init__(self, environment, q_learning_rate:float, discount_factor:float,
                  decaying_rate:float, epsilon:float, model_type:tf.keras.Model,
                  criterion, optimizer, net_learning_rate):
         super().__init__(environment, q_learning_rate, discount_factor,
                          decaying_rate, epsilon)
-        self.exp_reply_deque    = deque(maxlen=50000)
+        
+        # define the experience reply deque
+        self.maximal_reply_size = 10000
+        self.minimal_reply_size = 1000
+        self.exp_reply_deque    = deque(maxlen=self.maximal_reply_size)
+        
+        # initialize models and equalize weights
         self.main_model         = self.model_init(model_type, criterion, optimizer, net_learning_rate)
         self.target_model       = self.model_init(model_type, criterion, optimizer, net_learning_rate)
+        self.assign_weights()
         
-    def input_reshape(self, input_state):
+    def assign_weights(self):
+        main_model_weights = self.get_weights()
+        self.target_model.set_weights(main_model_weights)
+    
+    def model_input_reshape(self, input_state):
         """ reshape single state input as a model input
 
         Args:
@@ -100,13 +96,16 @@ class DeepQLearning(QLearning):
     
     def model_init(self, model_type, criterion, optimizer, net_learning_rate):
         model = model_type(self.dim[0], self.dim[1])
-        model.compile(loss= criterion, optimizer=optimizer(net_learning_rate), metrics=['mse'])
+        model.compile(optimizer=optimizer(net_learning_rate),
+                      loss= criterion, 
+                      metrics=[tf.keras.metrics.MeanSquaredError])
+                    #   metrics=['mse'])
         return model
     
     def sample_action(self, current_state):
         odd = np.random.choice(2, 1, p=[self.epsilon, 1 - self.epsilon])
         if odd == 1:
-            return np.argmax(self.main_model.predict(self.input_reshape(current_state)).flatten()) 
+            return np.argmax(self.main_model.predict(self.model_input_reshape(current_state)).flatten()) 
         else:
             return self.environment.action_space.sample()
 
@@ -123,6 +122,154 @@ class DeepQLearning(QLearning):
             target = reward
         return target
     
-    def train_agent(self, batch_size):
-        # consider to apply condition on the size of the experience reply deque
-      
+    def q_update(self, current_state, current_action, target):
+        # calculate current model "q" function
+        model_q_value = self.main_model.predict(self.model_input_reshape(current_state)).flatten()
+        
+        # calculate the modified Bellman equation given main model output
+        q = (1 - self.q_learning_rate) * model_q_value[current_action] + self.q_learning_rate * target
+        
+        # update the model current action output
+        model_q_value[current_action] = q
+        return model_q_value[current_action]
+    
+    def generate_training_database(self, batch_size):
+        """generates datasets for the main model training \
+           where the input data is the current states and the "labels" \
+           are updated modified q values, based on Bellman equation
+
+        Args:
+            batch_size (int): size of sampled mini-batch (from experience reply deque)
+
+        Returns:
+            observations (NDArray): observations dataset
+            q_values     (NDArray): modified q values dataset
+        """        
+        observations = []
+        q_values     = []
+        
+        mini_batch = self.sample_batch(batch_size)
+        
+        for exp_sample in mini_batch:
+            # assign variables for iteration and model
+            current_state, current_action, reward, done, next_state = exp_sample
+            observations.append(current_state)
+            
+            # get target based on target model
+            target = self.get_target(next_state, reward, done)
+            
+            # update the model current action i.e. modified q-value and append to q_values
+            q_values.append(self.q_update(current_state, current_action, target))
+        
+        return np.array(observations), np.array(q_values) 
+        
+    def train(self, batch_size, epochs):
+        if len(self.exp_reply_deque) >= self.minimal_reply_size:
+            # generate datasets for training
+            observations, q_values = self.generate_training_database(batch_size)
+            # train model
+            self.main_model.fit(x=observations, y=q_values,
+                                batch_size=batch_size,
+                                epochs=epochs,
+                                shuffle=True)
+    
+    def test_agent(self, state):
+        action = np.argmax(self.main_model.predict(self.model_input_reshape(state)).flatten())
+        next_state, reward, done = self.env_step(action)
+    
+    def train_agent(self, num_of_episodes, weights_assign_num, training_num,
+                    batch_size = 64, epochs=10):
+        rewards          = []
+        averaged_steps   = []
+        averaged_rewards = []
+        for episode in range(num_of_episodes):
+        
+            # reset environment 
+            current_state = self.environment.env.reset()[0]
+            
+            # initialize episode parameters
+            done = False
+            step = 0
+            update_counter = 0
+            
+            # update epsilon value following decaying epsilon greedy method 
+            self.update_epsilon()
+            
+            while True:
+                # increment steps
+                step += 1
+                update_counter += 1
+                
+                # sample action based on epsilon-greedy method
+                current_action = self.sample_action(current_state)
+                
+                # apply environment step
+                next_state, reward, done = self.env_step(current_action)
+                
+                # enrich the experience reply deque
+                self.store_past_exp(current_state=current_state,
+                                    current_action= current_action,
+                                    reward=reward,
+                                    done=done,
+                                    next_state=next_state)
+                
+                # train the main model
+                if update_counter % training_num == 0:
+                    self.train(batch_size=batch_size, epochs=epochs)
+
+                # move on to the next step 
+                current_state = next_state
+                rewards.append(reward)
+                
+                # check if game terminated
+                if done:
+                    steps_of_hundred_episodes.append(step)
+                    if update_counter >= weights_assign_num:
+                        self.assign_weights()
+                        update_counter = 0
+                    break
+            
+            if (episode % 100 == 0) and (episode != 0):
+                # calculate average number of steps every 100 episodes: 
+                averaged_steps.append(np.mean(np.array(steps_of_hundred_episodes)))
+                steps_of_hundred_episodes = []
+
+                # calculate average reward every 100 episodes: 
+                rewards_of_hundred_episodes = rewards[-100:]
+                averaged_rewards.append(np.mean(np.array(rewards_of_hundred_episodes)))
+        
+        # end environment activity
+        self.environment.env.close()
+
+    def test_agent(self):
+        # reset environment
+        rewards = [] 
+        step = 0
+        current_state = self.environment.env.reset()[0]
+        done = False
+        plt.imshow(self.environment.env.render())
+        
+        while True:
+            # increment steps
+            step += 1
+            update_counter += 1
+            
+            # perform action based on policy
+            current_action = np.argmax(self.main_model.predict(self.model_input_reshape(current_state)).flatten())
+            
+            plt.imshow(self.environment.env.render())
+            
+            # apply environment step
+            next_state, reward, done = self.env_step(current_action)
+
+            # move on to the next step 
+            current_state = next_state
+            rewards.append(reward)
+            
+            # check if game terminated
+            if done:
+                print("game ended after {} steps, overall reward is {}".format(step, np.sum(rewards)))
+                break
+        
+        # end environment activity
+        self.environment.env.close()
