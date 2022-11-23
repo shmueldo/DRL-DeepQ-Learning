36ece7e45e7761ed430bce75819da439dcc95d90
Dor Haim Shmuel <shmueldo@post.bgu.ac.il>
2022-11-23 20:59:02 +0200

    q_update is now correct

--- Models.py
+++ Models.py
@@ -6,23 +6,28 @@ from tensorflow.keras.layers import Dense
 class ThreeLayersModel(tf.keras.Model):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.input_layer    = Dense(32, input_shape=input_dim, activation='relu')
-        self.hidden         = Dense(16, activation='relu')
+        self.input_layer    = Dense(4, input_shape=input_dim, activation='relu')
+        self.hidden_1       = Dense(16, activation='relu')
+        self.hidden_2       = Dense(64, activation='relu')
+        self.hidden_3       = Dense(16, activation='relu')
         self.output_layer   = Dense(output_dim, activation='linear')
 
     def call(self, inputs):
         x = self.input_layer(inputs)
-        x = self.hidden(x)
+        x = self.hidden_1(x)
+        x = self.hidden_2(x)
+        x = self.hidden_3(x)
         return self.output_layer(x)
 
 class FiveLayersModel(tf.keras.Model):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.input_layer    = Dense(256, input_shape=input_dim, activation='relu')
-        self.hidden_1       = Dense(128, activation='relu')
-        self.hidden_2       = Dense(64, activation='relu')
+        self.input_layer    = Dense(16, input_shape=input_dim, activation='relu')
+        self.hidden_1       = Dense(64, activation='relu')
+        self.hidden_2       = Dense(16, activation='relu')
         self.hidden_3       = Dense(32, activation='relu')
         self.hidden_4       = Dense(16, activation='relu')
+        self.hidden_5       = Dense(16, activation='relu')
         self.output_layer   = Dense(output_dim, activation='linear')
 
     def call(self, inputs):
@@ -31,6 +36,7 @@ class FiveLayersModel(tf.keras.Model):
         x = self.hidden_2(x)
         x = self.hidden_3(x)
         x = self.hidden_4(x)
+        x = self.hidden_5(x)
         return self.output_layer(x)
     
 if __name__ == "__main__":
