cf9ebd36fad019904fed16056819afac89fe6309
yuval5210 <yucoh@post.bgu.ac.il>
2022-11-27 19:45:03 +0200

    DDQN

--- QAlgorithms.py
+++ QAlgorithms.py
@@ -355,6 +355,100 @@ class DeepQLearning(QLearning):
         plt.show()
         self.environment.env.close()
 
+class DoubleDeepQLearning(DeepQLearning):
+    def __int__(self, environment, q_learning_rate, discount_factor, decaying_rate, epsilon, epsilon_min,
+                model_type:tf.keras.Model, criterion, optimizer, net_learning_rate):
+        super().__init__(environment, q_learning_rate, discount_factor, decaying_rate, epsilon)
+
+        self.nS = self.environment.observation_space.shape[0]
+        self.nA = self.environment.action_space.n
+        # define the experience reply deque
+        self.maximal_reply_size = 50000
+        self.minimal_reply_size = 256
+        self.exp_reply_deque = deque(maxlen=self.maximal_reply_size)
+        self.epsilon_min = epsilon_min
+
+        self.model = self.model_init(model_type=model_type, criterion=criterion, optimizer=optimizer, net_learning_rate=net_learning_rate)
+        self.model_target = self.model_init(model_type=model_type, criterion=criterion, optimizer=optimizer, net_learning_rate=net_learning_rate)
+        self.assign_weights()
+        self.loss = []
+        self.train_end = 0
+
+    def generate_training_database(self, batch_size):
+        # Execute the experience replay
+        minibatch = random.sample(self.exp_reply_deque, batch_size)  # Randomly sample from memory
+
+        # Convert to numpy for speed by vectorization
+        x = []
+        y = []
+        np_array = np.array(minibatch)
+        st = np.zeros((0, self.nS))  # States
+        nst = np.zeros((0, self.nS))  # Next States
+        for i in range(len(np_array)):  # Creating the state and next state np arrays
+            st = np.append(st, np_array[i, 0], axis=0)
+            nst = np.append(nst, np_array[i, 3], axis=0)
+        st_predict = self.model.predict(st)  # Here is the speedup! I can predict on the ENTIRE batch
+        nst_predict = self.model.predict(nst)
+        nst_predict_target = self.model_target.predict(nst)  # Predict from the TARGET
+        index = 0
+        for current_state, current_action, reward, done, next_state in minibatch:
+            x.append(current_state)
+            # Predict from state
+            nst_action_predict_target = nst_predict_target[index]
+            nst_action_predict_model = nst_predict[index]
+            target = self.get_target(done=done)
+            target_f = st_predict[index]
+            target_f[current_action] = target
+            y.append(target_f)
+            index += 1
+        # Reshape for Keras Fit
+        x_reshape = np.array(x).reshape(batch_size, self.nS)
+        y_reshape = np.array(y)
+        epoch_count = 1
+        hist = self.model.fit(x_reshape, y_reshape, epochs=epoch_count, verbose=0)
+        # Graph Losses
+        for i in range(epoch_count):
+            self.loss.append(hist.history['loss'][i])
+        # Decay Epsilon
+        if self.epsilon > self.epsilon_min:
+            self.update_epsilon()
+
+    def train_agent(self, num_of_episodes, weights_assign_num, training_num, batch_size=64, epochs=10):
+        rewards = []
+        epsilons = []
+        test_episodes = 0
+        for episode in range(num_of_episodes):
+            state = self.environment.reset()
+            state = np.reshape(state, [1, self.nS])  # Resize to store in memory to pass to .predict
+            tot_rewards = 0
+            for time in range(210):  # 200 is when you "solve" the game. This can continue forever as far as I know
+                action = self.sample_action(state)
+                next_state, reward, done, _ = self.environment.step(action)
+                next_state = np.reshape(next_state, [1, self.nS])
+                tot_rewards += reward
+                self.store_past_exp(state, action, reward, done, next_state)  # Resize to store in memory to pass to .predict
+                state = next_state
+                # done: CartPole fell.
+                # time == 209: CartPole stayed upright
+                if done or time == 209:
+                    rewards.append(tot_rewards)
+                    epsilons.append(self.epsilon)
+                    print("episode: {}/{}, score: {}, e: {}".format(episode, num_of_episodes, tot_rewards, dqn.epsilon))
+                    break
+                # Experience Replay
+                if len(self.exp_reply_deque) > batch_size:
+                    self.train(batch_size)
+            # Update the weights after each episode (You can configure this for x steps as well
+            self.assign_weights()
+            # If our current NN passes we are done
+            # I am going to use the last 5 runs
+            if len(rewards) > 5 and np.average(rewards[-5:]) > 195:
+                # Set the rest of the EPISODES for testing
+                test_episodes = num_of_episodes - episode
+                self.train_end = episode
+                break
+
+
 if __name__ == "__main__":
     cart_pole_env = CartPole()
     dqn = DeepQLearning(environment=cart_pole_env,
